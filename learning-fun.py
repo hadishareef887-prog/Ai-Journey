45eebd3d1f353a70e6cc4eecac3f9b7a889e919b
Mohammed Hadi <hadishareef887@gmail.com>
2026-01-04 12:27:55 +0530

    learning-fun

---
+++ learning-fun.py
@@ -0,0 +1,8 @@
+str1 = "hello to my journey. "
+str2 = "i am here to learn python"
+str1 = str1.capitalize()
+str2 = str2.capitalize()
+str3 = str1+str2
+print(str3)
+print(len(str3))
+print(str3.find("here"))
